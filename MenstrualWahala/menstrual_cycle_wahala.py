76a1683c18c4aa8d442448e66c5b4c1172782656
yusufusman <yusuf.usman1003@gmail.com>
2025-04-08 22:18:44 +0100

    menstrual wahala added

--- MenstrualWahala/menstrual_cycle_wahala.py
+++ MenstrualWahala/menstrual_cycle_wahala.py
@@ -27,11 +27,11 @@ class MenstrualCycleWahala:
 
 
     def get_safe_period1_start(self):
-        return self.last_period_date()
+        return self.last_period_date
 
 
     def get_safe_period1_end(self):
-        return self.get_safe_period1_start() - timedelta(days=1)
+        return self.get_fertile_period_start() - timedelta(days=1)
 
 
     def get_safe_period2_start(self):
@@ -58,30 +58,40 @@ def main():
             print("Invalid date")
 
 
-        while True:
-            try:
-                cycle_length = int(input("Enter menstrual cycle length (e.g., 28 days): "))
-                if cycle_length <= 0:
-                    print("Menstrual cycle length must be a positive integer")
-                    continue
-                break
-            except ValueError:
-                print("Invalid number. Please enter a valid integer")
+    while True:
+        try:
+            cycle_length = int(input("Enter menstrual cycle length (e.g., 28 days): "))
+            if cycle_length <= 0:
+                print("Menstrual cycle length must be a positive integer")
+                continue
+            break
+        except ValueError:
+            print("Invalid number. Please enter a valid integer")
 
+    while True:
+        try:
+            period_length = int(input("Enter menstrual period length (e.g., 5 days): "))
+            if period_length <= 0:
+                print("Menstrual period length must be a positive integer")
+                continue
+            break
+        except ValueError:
+            print("Invalid number. Please enter a valid integer")
 
-        while True:
-            try:
-                period_length = int(input("Enter menstrual period length (e.g., 5 days): "))
-                if period_length <= 0:
-                    print("Menstrual period length must be a positive integer")
-                    continue
-                break
-            except ValueError:
-                print("Invalid number. Please enter a valid integer")
+    calculate = MenstrualCycleWahala(last_period, cycle_length, period_length)
+    date_format = "%Y-%m-%d"
 
-            calculator = MenstrualCycleWahala(last_period, cycle_length, period_length)
+    print("=========================================================================================")
+    print("\nMENSTRUAL CYCLE APP Result")
+    print("=========================================================================================")
+    print(f"Next Period: {calculate.get_next_period_start().strftime(date_format)} to "f"{calculate.get_next_period_end().strftime(date_format)}")
+    print(f"Ovulation Date: {calculate.get_ovulation_period().strftime(date_format)}")
+    print(f"Fertile Range: {calculate.get_fertile_period_start().strftime(date_format)} to "
+          f"{calculate.get_fertile_period_end().strftime(date_format)}")
+    print(f"Safe Period 1: {calculate.get_safe_period1_start().strftime(date_format)} to "
+          f"{calculate.get_safe_period1_end().strftime(date_format)}")
+    print(f"Safe Period 2: {calculate.get_safe_period2_start().strftime(date_format)} to "
+          f"{calculate.get_safe_period2_end().strftime(date_format)}")
 
-
-
-
-    
+if __name__ == "__main__":
+    main()
